a354fc6b803d3d5f660601df7ee5ce90d4eeb0d9
Ragnarök <113249084+TheBedrockEnder@users.noreply.github.com>
2025-03-17 11:01:30 +0000

    e

--- musicgame.py
+++ musicgame.py
@@ -1,2 +1,5 @@
+import random
+
 print("Welcome to the music game!")
-Music = [["Aritst A", "Song A"],["Artist B", "Song B"]]
+Music = [["Aritst A", "Song A", "Song A 2"],["Artist B", "Song B"]]
+print (random.choice(Music))
