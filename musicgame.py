96dcec9fcfb053e0bc1e36b2a83eb057d06d0ec2
Ragnarök <113249084+TheBedrockEnder@users.noreply.github.com>
2025-03-17 11:30:07 +0000

    Finished Music game

--- musicgame.py
+++ musicgame.py
@@ -1,5 +1,28 @@
 import random
 
 print("Welcome to the music game!")
+
 Music = [["Aritst A", "Song A", "Song A 2"],["Artist B", "Song B"]]
-print (random.choice(Music))
+
+Guess_Number = 0
+Guess = ""
+points = 0
+while Guess_Number < 2:
+    if Guess_Number == 0:
+        Music_random = (random.choice(Music))
+        Music_artist = Music_random.pop(0)
+        Song = random.randint(0, len(Music_random)-1)
+        print("Artist: ", Music_artist, "Song: ", Music_random[Song][0])
+    Guess = input("What is the name of the song? ")
+    if Guess == Music_random[Song]:
+        print("Correct!")
+        if Guess_Number == 0:
+            points += 3
+        else:
+            points += 1
+        Guess_Number = 0
+    else:
+        Guess_Number += 1
+        print("Wrong! you have ", 2-Guess_Number, "guesses left")
+
+print ("Game over! You got ", points, "point(s)")
