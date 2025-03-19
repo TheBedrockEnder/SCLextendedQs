2a3d0c8eff1c88d911272a99eefc63bb39b75509
Ragnarök <113249084+TheBedrockEnder@users.noreply.github.com>
2025-03-19 11:12:51 +0000

    fixed bugs

--- musicgame.py
+++ musicgame.py
@@ -1,18 +1,22 @@
 import random
-HighScore = open("musicgamehighscores.txt", "r")
+HighScore = open("musicgamehighscores.txt", "r+")
+HighScoreRead = HighScore.read()
 
 print("Welcome to the music game!")
 
 name = input("Enter your name: ")
 
-Music = [["Aritst A", "Song A", "Song A 2"],["Artist B", "Song B"]]
+Music = [["Aritst A", "Song A", "Song A 2"],["Artist B", "Song B", "Song B 2"],["Artist C", "Song C", "Song C 2"]]
 
 Guess_Number = 0
 Guess = ""
 points = 0
 while Guess_Number < 2:
     if Guess_Number == 0:
-        Music_random = (random.choice(Music))
+        Music_copy = [list(artist) for artist in Music]
+        Music_random = (random.choice(Music_copy))
+        if len(Music_random) == 0:
+            continue
         Music_artist = Music_random.pop(0)
         Song = random.randint(0, len(Music_random)-1)
         print("Artist: ", Music_artist, "Song: ", Music_random[Song][0])
@@ -29,3 +33,6 @@ while Guess_Number < 2:
         print("Wrong! you have ", 2-Guess_Number, "guesses left")
 
 print ("Game over! You got ", points, "point(s)")
+
+HighScore.write(name + " " + str(points) + "\n")
+print("High Scores = \n", HighScoreRead)
